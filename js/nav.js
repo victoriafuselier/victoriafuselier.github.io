f04859d99882e4e3702865df625df6121978db11
Victoria Fuselier <vic.fuselier@gmail.com>
2024-04-26 01:22:19 -0500

    Attempting to vanish theme/language btn on secondary pages
    I would like them to either fade in opacity or slide up out of view. I'd also like to animate the transition. Not sure how to do this exactly so I'm just saving my first attempt code to try again later. May need to review how it works with the transition to page function

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,7 @@ const navButton = document.querySelector('#nav-button');
 const navDiv = document.querySelector('.nav');
 const main = document.querySelector('main'); 
 const footer = document.querySelector('footer');
+const body = document.querySelector('body');
 
 navButton.addEventListener('click', () => {
     const isOpened = navButton.getAttribute('aria-expanded');
@@ -17,6 +18,15 @@ navButton.addEventListener('click', () => {
         footer.style.display = 'block';   
     }
 })
+
+body.addEventListener('load', ()=> {
+    if (main.classList.contains('about') || main.classList.contains('work') || main.classList.contains('contact')) {
+        const themeButton = document.querySelector('.theme-switch-container');
+        const languageButton = document.querySelector('.language-switch-container');
+        themeButton.style.transform = "translateY(10px)";
+
+    }
+});
 
 window.transitionToPage = function(href) {
     document.querySelector('body').style.opacity = 0
