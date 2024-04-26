8ae6e3a41f3eba9deeee9104b06cd0f3b09a5446
Victoria Fuselier <vic.fuselier@gmail.com>
2024-04-25 23:01:57 -0500

    Removed footer from secondary pages; hid scrollbar
    I also attempted to get dark mode to persist across pages; needs more work

--- js/theme.js
+++ js/theme.js
@@ -1,15 +1,5 @@
 const contactIconArray = document.getElementsByClassName('contact-icons');
 
-function setLightMode() {
-    const body = document.querySelector('body');
-    body.classList.add('light');
-    body.classList.remove('dark');
-    logo.src = 'images/icons/logo-dark.png';
-    for (let i = 0; i < contactIconArray.length; i++) {
-        contactIconArray[i].style.fill = "#163746";
-    }
-};
-
 function setDarkMode() {
     const body = document.querySelector('body');
     body.classList.remove('light');
@@ -18,32 +8,46 @@ function setDarkMode() {
     for (let i = 0; i < contactIconArray.length; i++) {
         contactIconArray[i].style.fill = "#edeff0";
     }
-};
+    localStorage.setItem('theme', 'dark');
+}
 
-function checkTheme() {
-    const theme = localStorage.getItem('theme');
-    if (theme === 'light') {
-        setLightMode();        
-    } else if (theme === 'dark') {
-        setDarkMode();
+function setLightMode() {
+    const body = document.querySelector('body');
+    body.classList.add('light');
+    body.classList.remove('dark');
+    logo.src = 'images/icons/logo-dark.png';
+    for (let i = 0; i < contactIconArray.length; i++) {
+        contactIconArray[i].style.fill = "#163746";
     }
-};
+    localStorage.setItem('theme', 'light');
+}
 
 document.addEventListener('onload', () => {
-    checkTheme();
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+        setDarkMode();
+    } else {
+        setLightMode();
+    }
 });
 
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener("DOMContentLoaded", () => {
     const themeSwitch = document.querySelector('.theme-switch-container');
     const body = document.querySelector('body');
     themeSwitch.addEventListener('click', () => {
         if (body.classList.contains('light')) {
+            localStorage.clear();
+            localStorage.setItem('theme', 'dark');
             setDarkMode();
         } else if (body.classList.contains('dark')) {
+            localStorage.clear();
+            localStorage.setItem('theme', 'light');
             setLightMode();
         }
     });
 });
+
+
 
 
 
