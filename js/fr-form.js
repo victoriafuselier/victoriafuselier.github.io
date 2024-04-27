a8f01d7cd254ab8618d041efd41c5e3176e7cf16
Victoria Fuselier <vic.fuselier@gmail.com>
2024-04-26 22:34:49 -0500

    Copied form.js changes to fr-form.js
    I still have to test it on live site to test it. Seemed to work when testing on local site

--- js/fr-form.js
+++ js/fr-form.js
@@ -3,39 +3,6 @@ const userName = document.getElementById('name');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
 
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	
-	checkInputs();
-});
-
-function checkInputs() {
-	// trim to remove the whitespaces
-	const userNameValue = userName.value.trim();
-	const emailValue = email.value.trim();
-	const messageValue = message.value.trim();
-	
-	if(userNameValue === '') {
-		setErrorFor(userName, '*Le champ ne peut pas être vide');
-	} else {
-		setSuccessFor(userName);
-	}
-	
-	if(emailValue === '') {
-		setErrorFor(email, '*Le champ ne peut pas être vide');
-	} else if (!isEmail(emailValue)) {
-		setErrorFor(email, '*Ce n\'est pas un e-mail valid');
-	} else {
-		setSuccessFor(email);
-	}
-	
-	if(messageValue === '') {
-		setErrorFor(message, '*Le champ ne peut pas être vide');
-	} else {
-		setSuccessFor(message);
-	}
-}
-
 function setErrorFor(input, errorMessage) {
 	const formControl = input.parentElement;
 	const small = formControl.querySelector('small');
@@ -51,3 +18,48 @@ function setSuccessFor(input) {
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+
+function checkInputs() {
+	const userNameValue = userName.value.trim();
+	const emailValue = email.value.trim();
+	const messageValue = message.value.trim();
+	
+	if (userNameValue === '') {
+		setErrorFor(userName, '*Le champ ne peut pas être vide');
+	} else {
+		setSuccessFor(userName);
+	}
+	
+	if (emailValue === '') {
+		setErrorFor(email, '*Le champ ne peut pas être vide');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, '*Ce n\'est pas un e-mail valid');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if (messageValue === '') {
+		setErrorFor(message, '*Le champ ne peut pas être vide');
+	} else {
+		setSuccessFor(message);
+	}
+}
+
+function validateForm() {
+	if (userName.parentElement.classList.contains("success") && email.parentElement.classList.contains("success") && message.parentElement.classList.contains("success")) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+form.addEventListener("submit", function(event) {
+	event.preventDefault();
+	checkInputs();
+	validateForm();
+	if (validateForm()) {
+	  this.submit();
+	  alert("Your message has been sent. Thanks!");
+	}
+});
+  
