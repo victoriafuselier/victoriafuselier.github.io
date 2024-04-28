9c4a9c40a7db16948a54b75b369cbfc5b7939827
Victoria Fuselier <vic.fuselier@gmail.com>
2024-04-28 18:19:40 -0500

    More work on both nav and page transitions

--- js/nav.js
+++ js/nav.js
@@ -11,50 +11,74 @@ const header = document.querySelector("header");
 navButton.addEventListener('click', () => {
     const isOpened = navButton.getAttribute('aria-expanded');
     if (isOpened === 'false') {
-        navButton.setAttribute('aria-expanded', 'true');
         main.style.opacity = 0;
         if (document.querySelector('footer') !== null) {
-            footer.style.bottom = '-100%';
+            footer.style.bottom = '-6vh';
+            footer.style.opacity = 0;
         }
-        languageSwitchContainer.style.opacity = 0;
-        themeSwitchContainer.style.opacity = 0;
+        if (window.location.href.includes('index.html') || window.location.href.includes('fr-index.html')) {
+            languageSwitchContainer.style.opacity = 0;
+            themeSwitchContainer.style.opacity = 0;
+        }
         setTimeout(function() {
-            navDiv.style.display ='block';
-            navDiv.style.opacity = 1;
             main.style.display = 'none';
-            main.style.opacity = 0;
             themeSwitchContainer.style.display = 'none';
             languageSwitchContainer.style.display = 'none';
+            navDiv.style.display ='block';
+            navButton.setAttribute('aria-expanded', 'true');
         }, 1000)
+        setTimeout(function() {
+            navDiv.style.opacity = 1;
+        }, 1500)
     } else {
-        navButton.setAttribute('aria-expanded', 'false');
         navDiv.style.opacity = 0;
-        themeSwitchContainer.style.display = 'block';
-        languageSwitchContainer.style.display = 'block';
-        if (document.querySelector('footer') !== null) {
-            footer.style.display = 'block';
-            footer.style.bottom = '6vh';
-        }
         setTimeout(function() {   
+            navDiv.style.display = 'none';
+            navButton.setAttribute('aria-expanded', 'false');
+            main.style.display = 'flex';
+            if (window.location.href.includes('index.html') || window.location.href.includes('fr-index.html')) {
+                themeSwitchContainer.style.display = 'block';
+                languageSwitchContainer.style.display = 'block';
+            }
+        }, 1000)
+        setTimeout(function() {
+            main.style.opacity = 1;
+            if (document.querySelector('footer') !== null) {
+                footer.style.bottom = '6vh';
+                footer.style.opacity = 1;
+            }
             if (window.location.href.includes('index.html') || window.location.href.includes('fr-index.html')) {
                 languageSwitchContainer.style.opacity = 1;
                 themeSwitchContainer.style.opacity = 1;
             }
-            main.style.display = 'flex';
-            main.style.opacity = 1; 
-        }, 1000)
+        }, 1500)
     }
 })
 
 window.transitionToPage = function(href) {
-    document.querySelector('body').style.opacity = 0
-    setTimeout(function() { 
-        window.location.href = href
+    document.querySelector('body').style.opacity = 0;
+    header.style.top = '-6vh';
+    if (footer !== null) {
+        footer.style.bottom = '-6vh';
+    }
+    if (navButton.getAttribute('aria-expanded') === 'true') {
+        navButton.setAttribute('aria-expanded', 'false');
+        navDiv.style.opacity = 0;
+        setTimeout(function() { 
+            navDiv.style.display = 'none';
+            body.style.display = 'block';
     }, 1000)
+    }
+    setTimeout(function() { 
+        window.location.href = href;
+    }, 1000)
+    setTimeout(function() { 
+        document.querySelector('body').style.opacity = 1;
+    }, 1500)
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('main').style.opacity = 1;
+    document.querySelector('body').style.opacity = 1;
     header.style.top = '6vh';
     if (footer !== null) {
         footer.style.bottom = '6vh';
