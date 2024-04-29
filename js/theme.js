40502073f141c636db4f8d5a5552a2c079193f43
Victoria Fuselier <vic.fuselier@gmail.com>
2024-04-29 11:45:36 -0500

    Fixed theme switch issue on home page
    Rearranged theme.js to change theme switch inside of set light mode/set dark mode functions so that it functions properly

--- js/theme.js
+++ js/theme.js
@@ -2,22 +2,32 @@ const contactIconArray = document.getElementsByClassName('contact-icons');
 
 function setDarkMode() {
     const body = document.querySelector('body');
+    const themeSwitch = document.querySelector('#theme-switch');
     body.classList.remove('light');
     body.classList.add('dark');
     logo.src = 'images/icons/logo-light.png';
     for (let i = 0; i < contactIconArray.length; i++) {
         contactIconArray[i].style.fill = "#edeff0";
     }
+    if (themeSwitch.classList.contains('fa-moon')) {
+        themeSwitch.classList.remove('fa-moon');
+        themeSwitch.classList.add('fa-sun');
+    }
     localStorage.setItem('theme', 'dark');
 }
 
 function setLightMode() {
     const body = document.querySelector('body');
+    const themeSwitch = document.querySelector('#theme-switch');
     body.classList.add('light');
     body.classList.remove('dark');
     logo.src = 'images/icons/logo-dark.png';
     for (let i = 0; i < contactIconArray.length; i++) {
         contactIconArray[i].style.fill = "#163746";
+    }
+    if (themeSwitch.classList.contains('fa-sun')) {
+        themeSwitch.classList.remove('fa-sun');
+        themeSwitch.classList.add('fa-moon');
     }
     localStorage.setItem('theme', 'light');
 }
@@ -44,25 +54,16 @@ window.addEventListener('load', (e) => {
 
 window.addEventListener("DOMContentLoaded", () => {
     const body = document.querySelector('body');
-    const themeSwitch = document.querySelector('#theme-switch');
     if (window.location.href.includes("index.html") || window.location.href.includes("fr-index.html")) {
         themeSwitchContainer.addEventListener('click', () => {
             if (body.classList.contains('light')) {
                 localStorage.clear();
                 localStorage.setItem('theme', 'dark');
                 setDarkMode();
-                if (themeSwitch.classList.contains('fa-moon')) {
-                    themeSwitch.classList.remove('fa-moon');
-                }
-                themeSwitch.classList.add('fa-sun');
             } else if (body.classList.contains('dark')) {
                 localStorage.clear();
                 localStorage.setItem('theme', 'light');
                 setLightMode();
-                if (themeSwitch.classList.contains('fa-sun')) {
-                    themeSwitch.classList.remove('fa-sun');
-                }
-                themeSwitch.classList.add('fa-moon');
             }
         });
     }
