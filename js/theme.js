ab65ccbad646149d89e9ef7f152853b2ca838ff4
Victoria Fuselier <vic.fuselier@gmail.com>
2024-05-02 17:52:59 -0500

    Fixing error on theme.js for theme switch function
    1. Had a ton of changes detected in Github after opening laptop for first time today. Unsure of how these appeared.. Live site was functioning just fine last night. Can only guess that I forgot to commit changes somehow and maybe something happened with OneDrive update overnight??? I have no idea but I will continue to investigate because I'd like this to never happen again. Deleted the 300 changes and reevaluated from there
    
    2. Error on theme.js seemed to be that it was targeting theme switch container with no height, instead of the <i> element holding the icon. Changed event listener to target icon itself instead of container

--- js/theme.js
+++ js/theme.js
@@ -44,8 +44,9 @@ window.addEventListener('load', (e) => {
 
 window.addEventListener("DOMContentLoaded", () => {
     const body = document.querySelector('body');
+    const themeSwitch = document.querySelector('#theme-switch');
     if (window.location.href.includes("index.html") || window.location.href.includes("fr-index.html")) {
-        themeSwitchContainer.addEventListener('click', () => {
+        themeSwitch.addEventListener('click', () => {
             if (body.classList.contains('light')) {
                 localStorage.clear();
                 localStorage.setItem('theme', 'dark');
