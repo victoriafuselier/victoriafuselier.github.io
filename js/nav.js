18b19e79c55b02cd929687af5f86a9facedd7d20
Victoria Fuselier <vic.fuselier@gmail.com>
2024-05-02 19:54:56 -0500

    Adjusted nav.js 'if' statement to fix button display error
    Cleaned up nav.js by deleting old commented code
    
    Switched theme switch target to theme switch. I have been going back and forth with this and need to ask a seasoned developer how best to approach this issue with targeting an icon by font awesome that actually displays icons using pseudo elements. It's very confusing. I can also research how to target pseudo elements with JS
    
    Also adjusted nav button event listener. If statement that displays theme and language switches wasn't working on first load of home page because if statement required 'index.html' or 'fr-index.html' to be location. After much confusion, I realized first load of page does not include index.html. It's simply 'victoriafuselier.github.io' so I adjusted the if statement
    
    Will test these changes on live site

--- js/nav.js
+++ js/nav.js
@@ -7,24 +7,15 @@ const languageSwitchContainer = document.querySelector('#language-switch-contain
 const themeSwitchContainer = document.querySelector('#theme-switch-container');
 const navButtonContainer = document.querySelector('#nav-button-container');
 const header = document.querySelector("header");
-const logo = document.querySelector('#logo');
 
 navButtonContainer.addEventListener('click', () => {
     const isOpened = navButton.getAttribute('aria-expanded');
     if (isOpened === 'false') {
         body.style.opacity = 0;
-        // main.style.opacity = 0;
-        // navButton.style.opacity = 0;
-        // logo.style.opacity = 0;
         header.style.top = '-6vh';
         if (footer !== null) {
             footer.style.bottom = '-6vh';
-            // footer.style.opacity = 0;
         }
-        // if (window.location.href.includes('index.html') || window.location.href.includes('fr-index.html')) {
-        //     languageSwitchContainer.style.opacity = 0;
-        //     themeSwitchContainer.style.opacity = 0;
-        // }
         setTimeout(function() {
             main.style.display = 'none';
             themeSwitchContainer.style.display = 'none';
@@ -36,24 +27,13 @@ navButtonContainer.addEventListener('click', () => {
             navDiv.style.display ='block';
         }, 1000);
         setTimeout(function() {
-            // navDiv.style.opacity = 1;
-            // navButton.style.opacity = 1;
-            // logo.style.opacity = 1;
             header.style.top = '6vh';
             body.style.opacity = 1;
             navDiv.style.opacity = 1;
         }, 1100);
-        // setTimeout(function() {
-        // }, 1200);
     } else {
-        // navDiv.style.opacity = 0;
-        // navButton.style.opacity = 0;
-        // logo.style.opacity = 0;
         body.style.opacity = 0;
         header.style.top = '-6vh';
-        if (footer !== null) {
-            footer.style.bottom = '-6vh';
-        }
         setTimeout(function() {   
             navButton.classList.remove('fa-xmark');
             navButton.classList.add('fa-bars');
@@ -61,25 +41,17 @@ navButtonContainer.addEventListener('click', () => {
             navButton.setAttribute('aria-expanded', 'false');
             navDiv.style.display = 'none';
             main.style.display = 'flex';
-            if (window.location.href.includes('index.html') || window.location.href.includes('fr-index.html')) {
+            if (window.location.href === 'https://victoriafuselier.github.io/' || window.location.href.includes('fr-index.html') || window.location.href.includes('index.html')) {
                 themeSwitchContainer.style.display = 'block';
                 languageSwitchContainer.style.display = 'block';
             }
         }, 1000)
         setTimeout(function() {
-            // main.style.opacity = 1;
-            // navButton.style.opacity = 1;
-            // logo.style.opacity = 1;
             body.style.opacity = 1;
             header.style.top = '6vh';
             if (footer !== null) {
                 footer.style.bottom = '6vh';
-                // footer.style.opacity = 1;
             }
-            // if (window.location.href.includes('index.html') || window.location.href.includes('fr-index.html')) {
-            //     languageSwitchContainer.style.opacity = 1;
-            //     themeSwitchContainer.style.opacity = 1;
-            // }
         }, 1100)
     }
 })
