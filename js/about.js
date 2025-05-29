6f3c2d33b86b49475c37fc614b2a558b4b59109d
Victoria Fuselier <vic.fuselier@gmail.com>
2025-05-29 17:08:03 -0500

    Fixed about pop-ups
    Best practice strategies seem to suggest not adding in loads of html using js innerhtml so I hardcoded it and toggled its display using js. I also had to adjust some CSS to reflect having only one pop up div with an id of that name, and both pop up boxes having a class of pop up box, etc. I also made the scrollbar on skills pop up visible, and can customize it later

--- js/about.js
+++ js/about.js
@@ -1,28 +1,19 @@
 const educationButton = document.querySelector('#education-button');
 const skillsButton = document.querySelector('#skills-button');
 const popUpDiv = document.querySelector('#pop-up-div');
+const skillsModal = document.querySelector('#skills-modal');
+const educationModal = document.querySelector('#education-modal');
 
 educationButton.addEventListener('click', () => {
     if (popUpDiv.classList.contains('hidden')) {
         popUpDiv.classList.remove('hidden');
     }
-
+    if (educationModal.classList.contains('hidden')) {
+        educationModal.classList.remove('hidden');
+    }
     popUpDiv.style.display = 'block';
     popUpDiv.style.height = '100%';
     popUpDiv.style.width = '100%';
-    
-    popUpDiv.innerHTML = `<div id='pop-up-box'>
-                            <button><i class="fa-solid fa-xl fa-xmark close"></i></button>
-                            <h2>Education</h2>
-                            <ul id="education">
-                                <li>Bachelor's Degree,<br>Louisiana State University</li>
-                                <li>FreeCodeCamp</li>
-                                <li>Front-End Web Development Track,<br>Treehouse</li>
-                                <li>Code Adventures,<br>Treehouse</li>
-                                <li><a target="_blank" href="../html/techdegree.html">Front-End Web Development Techdegree  <i class="fa-regular fa-file-pdf"></i></a></li>
-                            </ul>
-                        </div>`;
-
     let closeButton = document.querySelector('.close');
     closeButton.focus();
 });
@@ -31,75 +22,26 @@ skillsButton.addEventListener('click', () => {
     if (popUpDiv.classList.contains('hidden')) {
         popUpDiv.classList.remove('hidden');
     }
-
+    if (skillsModal.classList.contains('hidden')) {
+        skillsModal.classList.remove('hidden');
+    }
     popUpDiv.style.display = 'block';
     popUpDiv.style.height = '100%';
-    popUpDiv.style.width = '100%';
-    
-    popUpDiv.innerHTML =    
-                        `<div id="pop-up-box">
-                            <button><i class="fa-solid fa-xl fa-xmark close"></i></button>
-                            <h2>Skills</h2>
-                            <div id="skills">
-                                <ul>
-                                    <li>HTML</li>
-                                    <li>CSS</li>
-                                        <ul class="sublist">
-                                            <li>Grid</li>
-                                            <li>Flexbox</li>
-                                            <li>Transforms and Transitions</li>
-                                            <li>Keyframe Animations</li>
-                                            <li>Sass</li>
-                                            <ul>
-                                                <li class="sublist-level-2">and more!</li>
-                                            </ul>                            
-                                        </ul>
-                                    <li>JavaScript</li>
-                                        <ul class="sublist">
-                                            <li>DOM Selectors</li>
-                                            <li>DOM Manipulation</li>
-                                            <li>DOM Traversal</li>
-                                            <li>Callback functions</li>
-                                            <li>Array Methods</li>
-                                            <li>Array Iteration Methods</li>
-                                            <li>Object-Oriented JavaScript</li>
-                                            <li>Object Interactions</li>
-                                            <li>jQuery</li>
-                                            <li>Fetch API</li>
-                                                <ul>
-                                                    <li class="sublist-level-2">and more!</li>
-                                                </ul>
-                                        </ul>
-                                    <li>Web Design</li>
-                                    <li>Responsive Design</li>
-                                    <li>Accessibility</li>
-                                    <li>Bootstrap</li>
-                                    <li>Github</li>
-                                    <li>SVG Basics</li>
-                                    <li>AJAX Basics</li>
-                                    <li>Debugging with Chrome DevTools</li>
-                                </ul>
-                                <p class="pop-up-p">If you'd like to see a complete list of what I've been learning, check out my <a class="treehouse-link" href="https://teamtreehouse.com/profiles/victoriafuselier" target="_blank">Treehouse Profile</a>.</p>
-                            </div>
-                        </div>`;
-
+    popUpDiv.style.width = '100%';    
     let closeButton = document.querySelector('.close');
     closeButton.focus();                            
 });
 
 
 popUpDiv.addEventListener('click', (e) => {
-    if (e.target.classList.contains('close')) {
+    if (e.target.classList.contains('close') || e.target.id === 'pop-up-div') {
         popUpDiv.classList.add('hidden');
-        popUpDiv.innerHTML = ``;
-        popUpDiv.style.height = '0px';
-    }
-});
-
-popUpDiv.addEventListener('click', (e) => {
-    if (e.target.classList.contains('pop-up-div')) {
-        popUpDiv.classList.add('hidden');
-        popUpDiv.innerHTML = ``;
+        if (!educationModal.classList.contains('hidden')) {
+            educationModal.classList.add('hidden');
+        }
+        if (!skillsModal.classList.contains('hidden')) {
+            skillsModal.classList.add('hidden');
+        }
         popUpDiv.style.height = '0px';
     }
 });
